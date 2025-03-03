1e4d23ac4f9a3e2a7f4fe7394c57f4ad80075eee
$uname <$umail@gmail.com>
2025-03-03 11:36:04 +0000

    Update Chronon Effect Display

--- script.js
+++ script.js
@@ -81,17 +81,55 @@ function FillChrononModal(id) {
   var imageB64 = chrononData[id][0];
   var chrName = chrononData[id][1];
   var duration = chrononData[id][3];
-  var instaEff = chrononData[id][4].split("|");
-  var roundEff = chrononData[id][5].split("|");
-  var triggEff = chrononData[id][6].split("|");
+  var instaEff = chrononData[id][4].split("||");
+  var roundEff = chrononData[id][5].split("||");
+  var triggEff = chrononData[id][6].split("||");
 
   document.getElementById("chrononModalTitle").innerHTML = chrName;
   document.getElementById("chrononModalImage").innerHTML = `<img src="data:image/png;base64,${imageB64}">`;
   document.getElementById("chrononModalDuration").innerHTML = `Duration: ${duration} Round(s).`;
 
-  document.getElementById("chrononModalInsta").innerHTML = "<b>Instant Effect</b><br>" + GetSkillBadges(instaEff, "badge badge-success");
-  document.getElementById("chrononModalRound").innerHTML = "<b>Round Effect</b><br>" + GetSkillBadges(roundEff, "badge badge-primary");
-  document.getElementById("chrononModalTrigg").innerHTML = "<b>Trigger Skill</b><br>" + GetSkillBadges(triggEff, "badge badge-danger");
+  document.getElementById("chrononModalInsta").innerHTML = "<b>Instant Effect</b><br>" + CreateDesc(chrInsta, instaEff);
+  document.getElementById("chrononModalRound").innerHTML = "<b>Round Effect</b><br>" + CreateDesc(chrRound, roundEff);
+  document.getElementById("chrononModalTrigg").innerHTML = "<b>Trigger Skill</b><br>" + CreateDesc(chrTrigg, triggEff);
+}
+
+function CreateDesc(descData, values) {
+  if(values[0] == ""){
+    return "";
+  }
+  var descContent = "<ul>";
+  for(var i=0; i<values.length; i++){
+    var separatedValues = values[i].split("|");
+    var baseText = descData[ Number(separatedValues[0]) ];
+    console.log(baseText);
+    var formatVars = ["{0", "{1", "{2"];
+
+    for(var j=1; j<separatedValues.length; j++){
+      if(baseText.includes(formatVars[j-1])){
+        var currentHeaderFormat = formatVars[j-1];
+        var encodedValue = Number(separatedValues[j]);
+
+        if(baseText.includes(currentHeaderFormat + ":E}")){
+          encodedValue = attrValue[encodedValue];
+          baseText = baseText.replace(currentHeaderFormat + ":E}", encodedValue);
+        }
+        else{
+          if(baseText.includes(currentHeaderFormat + ":R}")){
+            encodedValue = raceValue[encodedValue];
+            baseText = baseText.replace(currentHeaderFormat + ":R}", encodedValue);
+          }
+          else{
+            baseText = baseText.replace(currentHeaderFormat + "}", encodedValue);
+          }
+        }
+
+      }
+    }
+    descContent += `<li>${baseText}</li>`;
+  }
+  descContent += "</ul>";
+  return descContent;
 }
 
 
