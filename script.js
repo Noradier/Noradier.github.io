4389b390fe80399b6e20bc55adbbcad9edf6bd4c
$uname <$umail@gmail.com>
2025-06-07 16:34:49 +0000

    Revamp Database

--- script.js
+++ script.js
@@ -51,15 +51,16 @@ function ShowChronons() {
 
 function WriteChrononContent() {
   document.getElementById("contentHeader").innerHTML = "<h1 class=\"text-center font-weight-bold\">Chronon Cards</h1>";
+  const chrononKeys = Object.keys(chrononCards);
 
   var t3 = "";
   var t2 = "";
   var t1 = "";
-
-  for(var i=0; i<chrononData.length; i++) {
-    var imageB64 = chrononData[i][0];
-    var chrTier = chrononData[i][2];
-    nextContent = `<a onclick="FillChrononModal(${i}); return false;" data-toggle="modal" href="#chrononModal">`;
+  
+  chrononKeys.forEach(chrononKey => {
+    var imageB64 = chrononCards[chrononKey].icon;
+    var chrTier = chrononCards[chrononKey].tier;
+    nextContent = `<a onclick="FillChrononModal(${chrononKey}); return false;" data-toggle="modal" href="#chrononModal">`;
     nextContent += `<img src="data:image/png;base64,${imageB64}" class="img-fluid custom-icon-size-2"></a>&nbsp;`;
     if(chrTier == 3) {
       t3 += nextContent;
@@ -70,28 +71,28 @@ function WriteChrononContent() {
     if(chrTier == 1) {
       t1 += nextContent;
     }
-  }
+  });
 
   newContent = `<table class="table-responsive-md table table-primary"><thead><th scope="col">Tier</th><th scope="col">Cards</th></tr></thead>`;
   newContent += `<tbody><tr><td>3</td><td>${t3}</td></tr><tr><td>2</td><td>${t2}</td></tr><tr><td>1</td><td>${t1}</td></tr></tbody></table>`;
   document.getElementById("content").innerHTML = newContent;
 }
 
-function FillChrononModal(id) {
-  var imageB64 = chrononData[id][0];
-  var chrName = chrononData[id][1];
-  var duration = chrononData[id][3];
-  var instaEff = chrononData[id][4].split("||");
-  var roundEff = chrononData[id][5].split("||");
-  var triggEff = chrononData[id][6].split("||");
+function FillChrononModal(key) {
+  var imageB64 = chrononCards[key].icon;
+  var chrName = chrononData[key][1].name;
+  var duration = chrononData[key][3].duration;
+  var instaEff = chrononData[key][4].instant.split("||");
+  var roundEff = chrononData[key][5].round.split("||");
+  var triggEff = chrononData[key][6].trigger.split("||");
 
   document.getElementById("chrononModalTitle").innerHTML = chrName;
   document.getElementById("chrononModalImage").innerHTML = `<img src="data:image/png;base64,${imageB64}">`;
   document.getElementById("chrononModalDuration").innerHTML = `Duration: ${duration} Round(s).`;
 
-  document.getElementById("chrononModalInsta").innerHTML = "<b>Instant Effect</b><br>" + CreateDesc(chrInsta, instaEff);
-  document.getElementById("chrononModalRound").innerHTML = "<b>Round Effect</b><br>" + CreateDesc(chrRound, roundEff);
-  document.getElementById("chrononModalTrigg").innerHTML = "<b>Trigger Skill</b><br>" + CreateDesc(chrTrigg, triggEff);
+  document.getElementById("chrononModalInsta").innerHTML = "<b>Instant Effect</b><br>" + CreateDesc(chrononSkills["instant"], instaEff);
+  document.getElementById("chrononModalRound").innerHTML = "<b>Round Effect</b><br>" + CreateDesc(chrononSkills["round"], roundEff);
+  document.getElementById("chrononModalTrigg").innerHTML = "<b>Trigger Skill</b><br>" + CreateDesc(chrononSkills["trigger"], triggEff);
 }
 
 function CreateDesc(descData, values) {
@@ -101,8 +102,7 @@ function CreateDesc(descData, values) {
   var descContent = "<ul>";
   for(var i=0; i<values.length; i++){
     var separatedValues = values[i].split("|");
-    var baseText = descData[ Number(separatedValues[0]) ];
-    console.log(baseText);
+    var baseText = descData[ separatedValues[0] ];
     var formatVars = ["{0", "{1", "{2"];
 
     for(var j=1; j<separatedValues.length; j++){
